893ca00a3a293caab6cf595930fdb660d917a136
Max Wittig <max.wittig95@gmail.com>
2018-06-06 20:09:48 +0200

    feat: add argument options

--- gitlab_languages.py
+++ gitlab_languages.py
@@ -2,9 +2,10 @@
 import argparse
 import os
 import gitlab
-from prometheus_client import Gauge, Counter, CollectorRegistry, write_to_textfile
+from prometheus_client import Gauge, Counter, \
+    CollectorRegistry, write_to_textfile
 import re
-from gitlab.exceptions import GitlabGetError
+from gitlab.exceptions import GitlabGetError, GitlabAuthenticationError
 import logging
 import itertools
 
@@ -12,7 +13,8 @@ logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
 log_level = logging.INFO
 handler.setLevel(log_level)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)s '
+                              '- %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(log_level)
@@ -49,7 +51,7 @@ class MetricsCollector:
     def add(self, language_name, percentage):
         if self.metrics.get(language_name):
             self.metrics[language_name] += LanguageMetrics(percentage)
-            logger.debug(f"Adding {percentage} to {language_name}")
+            logger.debug(f"Adding {percentage} to {language_name}")  # noqa
         else:
             self.metrics[language_name] = LanguageMetrics(percentage)
             logger.info(f"Adding new language {language_name}")
@@ -60,11 +62,14 @@ class MetricsCollector:
             logger.info(f"Adding {language_name} as Gauge")
             language_name = translate_language(language_name)
             language_name = re.sub("\W+", "", language_name)
-            gauge = Gauge(language_name, language_name, registry=self.registry)
+            gauge = Gauge(language_name, language_name,
+                          registry=self.registry)
             gauge.set(float(language) / total_languages)
 
         if total:
-            counter = Counter("total_projects_scanned", "Total projects scanned", registry=self.registry)
+            counter = Counter("total_projects_scanned",
+                              "Total projects scanned",
+                              registry=self.registry)
             counter.inc(total)
 
         logger.info("Writing languages to metrics.txt file")
@@ -73,27 +78,41 @@ class MetricsCollector:
 
 class LanguageScanner:
     def __init__(self, gitlab_url, access_token):
-        self.gl = gitlab.Gitlab(gitlab_url, private_token=access_token)
-        self.gl.auth()
+        self.gl = gitlab.Gitlab(gitlab_url,
+                                private_token=access_token)
+        try:
+            self.gl.auth()
+        except GitlabAuthenticationError:
+            exit("You're not authorized to access this GitLab "
+                 "instance.\nPlease check your access token.")
         self.metrics_collector = MetricsCollector()
 
-    def scan_projects(self, limit=None):
+    def scan_projects(self, limit=None, args=None):
         total_scanned = 0
-        projects = self.gl.projects.list(as_list=False, all_available=True)
+        if not args:
+            args = {}
+        projects = self.gl.projects.list(as_list=False,
+                                         all_available=True,
+                                         **args)
+
         if limit:
             projects = itertools.islice(projects, limit)
-            logger.info(f"Scanning {limit} projects")
+            logger.info(f"Scanning a maximum of {limit} projects")
         else:
             logger.info("Scanning all projects")
+
+        if args:
+            logger.info(f"with additional arguments {args}")
         for project in projects:
             logger.info(f"Scanning project {project.name}")
+
             try:
                 for language_name, percentage in project.languages().items():
                     self.metrics_collector.add(language_name, percentage)
                 total_scanned += 1
             except GitlabGetError as e:
                 logger.error(f"Failed to scan project {project.name}")
-                logger.error(e.error_message)
+                logger.debug(e.error_message)
 
         self.metrics_collector.write(total=total_scanned)
 
@@ -101,10 +120,18 @@ class LanguageScanner:
 def main():
     arg_parser = argparse.ArgumentParser("gitlab_languages")
     arg_parser.add_argument(
-        "--instanceurl", default="https://gitlab.com", required=False,
+        "--instanceurl", default="https://gitlab.com",
+        required=False, help="The url of your GitLab instance"
     )
     arg_parser.add_argument(
-        "--projectlimit", required=False,
+        "--projectlimit", default=None,
+        required=False,
+        help="Set project limit to scan"
+    )
+    arg_parser.add_argument(
+        "--args", default=None,
+        required=False,
+        help="Provide custom args to the GitLab API"
     )
     required_variables = ["GITLAB_ACCESS_TOKEN"]
     missing_variables = []
@@ -123,7 +150,18 @@ def main():
     gitlab_url = arguments.get("instanceurl")
     scanner = LanguageScanner(gitlab_url,
                               os.getenv("GITLAB_ACCESS_TOKEN"))
-    scanner.scan_projects(limit=int(arguments.get("projectlimit")))
+    additional_args = arguments.get("args")
+    additional_args_dict = {}
+    if additional_args:
+        for pair in additional_args.split(" "):
+            entry = pair.split("=")
+            additional_args_dict[entry[0]] = entry[1]
+
+    project_limit = arguments.get("projectlimit")
+    if project_limit:
+        project_limit = int(project_limit)
+    scanner.scan_projects(limit=project_limit,
+                          args=additional_args_dict)
 
 
 if __name__ == "__main__":
