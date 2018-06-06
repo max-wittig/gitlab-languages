f4c62d6921a75f404c5a27984a5c48108b0cf275
Max Wittig <max.wittig95@gmail.com>
2018-06-06 20:14:42 +0200

    chore(travis): add travis test run

--- setup.py
+++ setup.py
@@ -5,11 +5,8 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
 
 requirements = []
 
@@ -34,7 +31,7 @@ setup(
                 "GitLab instance, using the GitLab Language API",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='gitlab_languages',
     name='gitlab_languages',
