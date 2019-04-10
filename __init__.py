ae6ed16002cd145e7f52ea26cf49558ab1469ccf
Max Wittig <max.wittig95@gmail.com>
2019-04-10 22:34:53 +0200

    style: switch to black for linting

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@
 """Top-level package for gitlab_languages."""
 
 __author__ = """Max Wittig"""
-__email__ = 'max.wittig95@gmail.com'
-__version__ = '1.4.1'
+__email__ = "max.wittig95@gmail.com"
+__version__ = "1.4.1"
--- setup.py
+++ setup.py
@@ -8,49 +8,39 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = [
-    "prometheus_client>=0.3.1",
-    "python-gitlab>=1.6.0",
-    "maya>=0.5.0",
-]
+requirements = ["prometheus_client>=0.3.1", "python-gitlab>=1.6.0", "maya>=0.5.0"]
 
 setup_requirements = []
 
-test_requirements = [
-    "pycodestyle>=2.4.0",
-]
+test_requirements = ["pycodestyle>=2.4.0"]
 
 setup(
     author="Max Wittig",
-    author_email='max.wittig95@gmail.com',
+    author_email="max.wittig95@gmail.com",
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.6',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
     ],
     description="Utility to generate a Prometheus data source "
-                "text file for your GitLab repository "
-                "using the GitLab Language API",
+    "text file for your GitLab repository "
+    "using the GitLab Language API",
     install_requires=requirements,
     license="MIT license",
     long_description_content_type="text/markdown",
     long_description=readme,
     include_package_data=True,
     py_modules=["gitlab_languages"],
-    entry_points={
-        "console_scripts": [
-          "gitlab_languages=gitlab_languages:main"
-        ],
-    },
-    keywords='gitlab_languages',
-    name='gitlab_languages',
-    packages=find_packages(include=['gitlab_languages']),
+    entry_points={"console_scripts": ["gitlab_languages=gitlab_languages:main"]},
+    keywords="gitlab_languages",
+    name="gitlab_languages",
+    packages=find_packages(include=["gitlab_languages"]),
     setup_requires=setup_requirements,
     tests_require=test_requirements,
-    url='https://github.com/max-wittig/gitlab_languages',
-    version='1.4.1',
+    url="https://github.com/max-wittig/gitlab_languages",
+    version="1.4.1",
     zip_safe=False,
-    python_requires='>3.6.0',
+    python_requires=">3.6.0",
 )
